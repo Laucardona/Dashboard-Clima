482e25021368337ab0fd0e398402da481d13dba5
Laucardona <lauris.042006@gmail.com>
2026-03-03 13:24:12 -0500

    feat: (Api consumida de clima)

---
+++ main.js
@@ -0,0 +1,31 @@
+import { loadWeatherByCity, getServiceState } from "./service.js";
+import { render } from "./ui.js";
+
+const form = document.getElementById("searchForm");
+const input = document.getElementById("cityInput");
+
+function initApp() {
+    form.addEventListener("submit", handleSearch);
+    render({ loading: false, error: null, data: null });
+}
+
+async function handleSearch(e) {
+    e.preventDefault();
+
+    const city = input.value.trim();
+    if (!city) return;
+
+    // Mostrar loading
+    render({ loading: true });
+
+    const data = await loadWeatherByCity(city);
+    const { loading, error } = getServiceState();
+
+    render({
+        loading,
+        error,
+        data,
+    });
+}
+
+initApp();
---
+++ service.js
@@ -0,0 +1,52 @@
+const GEO_URL = "https://geocoding-api.open-meteo.com/v1/search";
+const WEATHER_URL = "https://api.open-meteo.com/v1/forecast";
+
+/* Estado interno */
+let state = {
+    loading: false,
+    error: null,
+};
+
+/* Obtener estado */
+export function getServiceState() {
+    return state;
+}
+
+/* Buscar clima por ciudad */
+export async function loadWeatherByCity(city) {
+    state.loading = true;
+    state.error = null;
+
+    try {
+        // 1️⃣ Coordenadas
+        const geoResponse = await fetch(`${GEO_URL}?name=${city}&count=1`);
+        const geoData = await geoResponse.json();
+
+        if (!geoData.results || geoData.results.length === 0) {
+            throw new Error("Ciudad no encontrada");
+        }
+
+        const { latitude, longitude, name } = geoData.results[0];
+
+        // 2️⃣ Clima actual
+        const weatherResponse = await fetch(
+            `${WEATHER_URL}?latitude=${latitude}&longitude=${longitude}&current_weather=true`
+        );
+
+        const weatherData = await weatherResponse.json();
+
+        if (!weatherData.current_weather) {
+            throw new Error("No hay datos del clima disponibles");
+        }
+
+        return {
+            city: name,
+            weather: weatherData.current_weather,
+        };
+
+    } catch (error) {
+        state.error = error.message;
+    } finally {
+        state.loading = false;
+    }
+}
